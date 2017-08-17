dc7d79f102ccadb87840a2100ca743048de58bf6
peak <peakdrum@github.com>
2017-08-17 08:21:45 +0700

    charge money with correct response

--- server.js
+++ server.js
@@ -5,59 +5,70 @@ const bodyParser = require('body-parser')
 const axios = require('axios')
 
 const dev = process.env.NODE_ENV !== 'production'
-const app = next({ dev })
+const app = next({
+	dev
+})
 const handle = app.getRequestHandler()
 const SECRET_KEY = 'skey_test_56u0cmouwglb4b9etxp'
 const EXPIRY_DATE = '2015-09-10'
+var omise = require('omise')({
+  'secretKey': SECRET_KEY,
+  'omiseVersion': EXPIRY_DATE
+});
 
 app.prepare()
-.then(() => {
-  const server = express()
+	.then(() => {
+		const server = express()
 
-  server.use(bodyParser.json())
-  server.use(bodyParser.urlencoded({extended: true}))
+		server.use(bodyParser.json())
+		server.use(bodyParser.urlencoded({
+			extended: true
+		}))
 
-  server.post('/api/charges', (req,res) => {
-    axios.post({
-      url:'https://api.omise.co/charges',
-      method: 'post',
-      auth: {
-        username: SECRET_KEY
-      },
-      data: {
-        description: req.body.description,
-        amount: req.body.amount,
-        currency: req.body.currency,
-        capture: req.body.capture,
-        card: req.body.card
-      }
-    }).then( function(response) {
-      console.log(response)
-    })
-  })
+		server.post('/api/charges', (req, res) => {
+			return omise.charges.create({
+				'description': req.body.description,
+				'amount': req.body.amount,
+				'currency': req.body.currency,
+				'capture': req.body.capture,
+				'card': req.body.card
+			}).then(function (charge) {
+				console.log('charge:', charge)
+				res.status(200).send(JSON.stringify(charge))
+			}).error(function (err) {
+				console.log('error:', err)
+			}).done()
 
-  server.get('/p/edit/:product_id/:user_id', (req, res) => {
-    const actualPage = '/productRegister'
-    const queryParams = { productID: req.params.product_id, userID: req.params.user_id } 
-    app.render(req, res, actualPage, queryParams)
-  })
+		})
 
-  server.get('/p/:product_id/:user_id', (req, res) => {
-    const actualPage = '/product'
-    const queryParams = { productID: req.params.product_id, userID: req.params.user_id } 
-    app.render(req, res, actualPage, queryParams)
-  })
+		server.get('/p/edit/:product_id/:user_id', (req, res) => {
+			const actualPage = '/productRegister'
+			const queryParams = {
+				productID: req.params.product_id,
+				userID: req.params.user_id
+			}
+			app.render(req, res, actualPage, queryParams)
+		})
 
-  server.get('*', (req, res) => {
-    return handle(req, res)
-  })
+		server.get('/p/:product_id/:user_id', (req, res) => {
+			const actualPage = '/product'
+			const queryParams = {
+				productID: req.params.product_id,
+				userID: req.params.user_id
+			}
+			app.render(req, res, actualPage, queryParams)
+		})
 
-  server.listen(3000, (err) => {
-    if (err) throw err
-    console.log('> Ready on http://localhost:3000')
-  })
-})
-.catch((ex) => {
-  console.error(ex.stack)
-  process.exit(1)
-})
+		server.get('*', (req, res) => {
+			return handle(req, res)
+		})
+
+		server.listen(3000, (err) => {
+			if (err) throw err
+			console.log('> Ready on http://localhost:3000')
+		})
+	})
+	.catch((ex) => {
+		console.error(ex.stack)
+		process.exit(1)
+	})
