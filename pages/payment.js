67d9f84268788693485e437094c79809e825e9f1
peak <peakdrum@github.com>
2017-09-03 15:09:40 +0700

    set up wallet sync

--- pages/payment.js
+++ pages/payment.js
@@ -5,15 +5,14 @@ import store from '../lib/store'
 import { reduxForm } from 'redux-form'
 import { createPayment } from '../lib/handlers/payment'
 
-let Payment = ({cardDetail, total}) => <PaymentView onCheckOut={()=>createPayment(total)}/>
+let Payment = ({cardDetail, total, sharedUser}) => <PaymentView onCheckOut={()=>createPayment(total, sharedUser.sharedUserUid)}/>
 
-Payment = reduxForm({
-	form: 'payment'
-})(Payment)
+Payment = reduxForm({form: 'payment'})(Payment)
 
 const mapStateToProps = state => ({
 	cardDetail : state.form.payment ? state.form.payment.values : null,
-	total : state.payment.total
+	total : state.payment.total,
+	sharedUser: state.sharedUser
 })
 
 export default withRedux(()=>store,mapStateToProps)(Payment)
