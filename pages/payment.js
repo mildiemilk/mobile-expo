8e74d05657d24635839d9e8e135c54daedee166c
peak <peakdrum@github.com>
2017-09-09 16:09:14 +0700

    record transction in redux state

--- pages/payment.js
+++ pages/payment.js
@@ -4,15 +4,53 @@ import withRedux from "next-redux-wrapper"
 import store from '../lib/store'
 import { reduxForm } from 'redux-form'
 import { createPayment } from '../lib/handlers/payment'
+import { addTransaction } from '../lib/actions/transaction'
+import { calculateComissionSeller } from '../lib/handlers/transaction'
 
-let Payment = ({cardDetail, total, sharedUser}) => <PaymentView onCheckOut={()=>createPayment(total, sharedUser.sharedUserUid)}/>
+class Payment extends React.Component{
+	componentDidMount() {
+			const { cart, products,  sharedUser, user, addTransaction } = this.props
+			cart.addedIds.forEach( id => {
+				let product = products[id]
+				let quantity = cart.quantityById[id]
+				let price = product.price
+				let comission = calculateComissionSeller(price, product.comissionCash, product.comissionPercent)
+				addTransaction({
+					price,
+					quantity,
+					comissionSeller:comission.seller * quantity,
+					comissionOwner: comission.owner * quantity,
+					sellerUid: sharedUser.sharedUserUid,
+					ownerUid:product.userUid,
+					buyerUid:user.uid,
+					productUid:id
+				})
+			});
+		} 
+
+	render() { 
+		const {cardDetail, total, sharedUser} = this.props
+		return <PaymentView 
+				onCheckOut={()=>createPayment(total, sharedUser.sharedUserUid)}
+			/>
+	}
+}
+
 
 Payment = reduxForm({form: 'payment'})(Payment)
 
 const mapStateToProps = state => ({
 	cardDetail : state.form.payment ? state.form.payment.values : null,
 	total : state.payment.total,
-	sharedUser: state.sharedUser
+	sharedUser: state.sharedUser,
+	products: state.userProducts,
+	cart : state.cart,
+	user: state.user,
+	transaction: state.transaction
 })
 
-export default withRedux(()=>store,mapStateToProps)(Payment)
+const mapDispatchToProps = {
+	addTransaction
+}
+
+export default withRedux(()=>store,mapStateToProps, mapDispatchToProps)(Payment)
