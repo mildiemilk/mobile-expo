747e95ca24e8b88881ea5b6d61a4873b937ddcc2
peak <peakdrum@github.com>
2017-09-15 17:15:55 +0700

    add button copy link alert

--- components/ItemCard.js
+++ components/ItemCard.js
@@ -1,39 +1,54 @@
-import { Card as SemanticCard, Button, Icon } from 'semantic-ui-react'
+import { Button, Icon } from 'semantic-ui-react'
 import Link from 'next/link'
 import copy from 'copy-to-clipboard';
 import styled from 'styled-components'
 
-const Card = styled(SemanticCard)`
-	width: auto !important;
-	margin: 10pt !important;
-	min-width: 320px !important;
+const Card = styled.div`
+	margin: 10pt;
+	max-width: 360px;
+	background-color: #f9f9f9; /* color behind the dashicon */
+	border: 2px solid #ccc; /* circle border width */
+	border-radius: 15px;
+	padding: 15px;
 `
 
-const Header = styled(SemanticCard.Header)`
+const Header = styled.h2`
 	width: auto;
 `
 
-const Content = styled(SemanticCard.Content)`
+const Content = styled.div`
 	width: auto;
 `
+
+const Img = styled.img`
+	min-width: 320px;
+	min-height: 240px;
+`
+
+const copyLink = (productKey, userUid)=>{
+	copy(`${window.location.hostname}:${window.location.port}/p/${productKey}/${userUid}`)
+	alert('the link has been copied')
+}
 
 export default ({ userUid, userProduct, productKey }) => {
 const { brandName, comissionCash, comissionPercent, price, priductDescription, productName} = userProduct
 
 return(
 	<Card>
-		<img alt="242x200" />
+		<Img alt="242x200" />
 		<Header>
-			<p>NAME: {productName} </p>
+			<h2>{productName} </h2>
 		</Header>
 		<Content>
-			<p>PRICE : {userProduct.price} baht </p>
-			<p>{userUid === userProduct.userUid ? 'product owner' : 'product seller'} </p>
-			<p>COMISSION : {comissionPercent} % & {comissionCash} BAHT (if owner can edit ) </p>
+			<p>Price: {userProduct.price} baht </p>
+			<p>Comission: {comissionPercent} % & {comissionCash} BAHT (if owner can edit ) </p>
 			<Button.Group>
+				{userUid === userProduct.userUid ?
 				<Link as={`/p/edit/${productKey}/${userUid}`} href={`/productRegister?productID=${productKey}&userID=${userUid}`}><Button basic color='blue'>Edit</Button></Link>
+				:null
+				}
 				<Link as={`/p/${productKey}/${userUid}`} href={`/product?productID=${productKey}&userID=${userUid}`}><Button basic color='green'>Preview</Button></Link>
-				<Button basic color='teal' onClick={()=>copy(`${window.location.hostname}:${window.location.port}/p/${productKey}/${userUid}`)}>GetLink</Button>
+				<Button basic color='teal' onClick={()=>copyLink(productKey, userUid)}>GetLink</Button>
 				<Button basic color='violet'>Share <i className="fa fa-share" aria-hidden="true"></i></Button>
 			</Button.Group>
 		</Content>
--- containers/Profile.js
+++ containers/Profile.js
@@ -3,27 +3,36 @@ import ItemCard from '../components/ItemCard'
 import Head from './DefaultHead'
 import Header from './Header'
 import { H3 } from '../components/Styled'
+import styled from 'styled-components'
+
+const Container = styled.div`
+	display: flex;
+	flex-flow: row wrap;
+	justify-content: space-around;
+	align-content: flex-start;
+`
+
+const Page = styled.div`
+`
 
 export default ({user, userProducts}) => <div>
 	<Head/>
 	<Header/>
 	{user.name}
-	<h2>{user.email}</h2>
-	<div>
-		<div>
-			<div>
-					{ userProducts ? 
-						Object.keys(userProducts).map( userProductKey => {
-							return (<ItemCard 
-								userProductKey={userProductKey} 
-								userUid={user.uid} 
-								userProduct={userProducts[userProductKey]} 
-								productKey={userProductKey}
-								key={userProductKey}
-							/>)
-						}) : null
-					}
-			</div>
-		</div>
-	</div>
+	<Page>
+		<h2>{user.email}</h2>
+		<Container>
+			{ userProducts ? 
+				Object.keys(userProducts).map( userProductKey => {
+					return (<ItemCard 
+						userProductKey={userProductKey} 
+						userUid={user.uid} 
+						userProduct={userProducts[userProductKey]} 
+						productKey={userProductKey}
+						key={userProductKey}
+					/>)
+				}) : null
+				}
+		</Container>
+	</Page>
 </div>
