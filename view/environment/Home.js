d17f3781300010f19967de04fb8cd2492b915342
peak <peakdrum@github.com>
2017-10-10 23:32:05 +0700

    remove most bug

--- view/environment/Home.js
+++ view/environment/Home.js
@@ -1,20 +1,10 @@
-import Link from 'next/link'
 import Head from './DefaultHead'
 import Header from './Header'
-import { Button } from 'semantic-ui-react'
-import styled from 'styled-components'
+import CompanyExplained from '../ecosystems/CompanyExplained'
 
 export default ({changePage, user}) => 
 <div>
   <Head title="Home"/>
   <Header />
-	<div>
-    <h1>Shop 'n Share</h1>
-    <p>แอพลิเคชั่นที่จะเปลี่ยนลูกค้าทุกคนของคุณเป็นตัวแทนจำหนาย และ</p>
-    <p>
-      <Link href="/about">
-      <Button color="blue">Learn more</Button>
-      </Link>
-    </p>
-  </div>
+  <CompanyExplained/>
 </div>
