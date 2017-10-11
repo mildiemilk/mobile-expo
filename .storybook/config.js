30580e1ee4ef55f4beaea173777cf70645dd4a07
peak <peakdrum@github.com>
2017-10-11 09:08:27 +0700

    add to storybook atom file

--- .storybook/config.js
+++ .storybook/config.js
@@ -1,7 +1,8 @@
 import { configure } from '@storybook/react';
 
 function loadStories() {
-  require('../src/stories');
+  require('../src/stories/atoms.stories.js');
+  // You can require as many stories as you need.
 }
 
 configure(loadStories, module);
