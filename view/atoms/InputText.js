03f0eba1b2afed727f6fe264d999e23524f01d5e
Peak Mac <peakdrum@gmail.com>
2017-10-14 20:06:36 +0700

    input style

--- view/atoms/InputText.js
+++ view/atoms/InputText.js
@@ -3,10 +3,16 @@ import styled from 'styled-components'
 export default styled.input`
 	width: -webkit-fill-available;
 	margin: 1%;
-	border: 1px #a9a9a9 solid;
 	padding: 10px;
 	color: #8f8d8b;
+	box-shadow: -1px 1px 1px #8f8d8b;
+	border-left: 2px solid #94002a;
+	border-bottom: 2px solid #94002a;
+	border-top: 1px solid #f1f1f1;
+	border-radius: 5px;
+	outline: none;
 	&:focus{
-		transition: 0.5s;
+		ttransition: .5s ease;
+		border: 2px #94002a solid;
 	}
 `
