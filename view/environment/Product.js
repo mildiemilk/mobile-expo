ac13f66d19b3525d103c1c5b1768b5aa41cc0193
Peak Mac <peakdrum@gmail.com>
2017-12-08 23:57:14 +0700

    refactor product form

--- view/environment/Product.js
+++ view/environment/Product.js
@@ -2,18 +2,11 @@ import Head from './DefaultHead'
 import Header from './Header'
 import ProductDisplay from '../ecosystems/ProductDisplay'
 
-export default ({product, minusQuantity, addQuantity, productUid, productQuantity}) => <div>
+export default props => <div>
 	<Head/>
 	<Header/>
 	<ProductDisplay 
-		images={product.productImages} 
-		productName={product.productName} 
-		shopName={product.brandName} 
-		price={product.price}
-		minusQuantity={minusQuantity}
-		addQuantity={addQuantity}		
-		onClickAdd={addQuantity} 
-		productUid={productUid} 
-		productQuantity={productQuantity}
+		{...props}
+		images={props.product.productImages} 
 	/>
 </div>
