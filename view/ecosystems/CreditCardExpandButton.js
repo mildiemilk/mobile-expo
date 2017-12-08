96312f2697618e9968df2632003d6a3b0cd4ab83
Peak Mac <peakdrum@gmail.com>
2017-12-08 18:40:11 +0700

    crete card button

---
+++ view/ecosystems/CreditCardExpandButton.js
@@ -0,0 +1,14 @@
+import Button from '../atoms/Button'
+import Wrapper from '../atoms/Wrapper'
+import ExpandTab from '../atoms/ExpandTab'
+import CreditCard from '../organisms/CreditCard'
+
+const ControlButton = props => <Button {...props} >จ่ายด้วยบัตร</Button>
+
+const InnerDisplay = props =>
+<Wrapper width="100%" maxWidth="512px">
+    <CreditCard {...props}/>
+</Wrapper>
+
+export default props => 
+<ExpandTab controlComponent={<ControlButton {...props}/>} innerComponent={<InnerDisplay />} />
