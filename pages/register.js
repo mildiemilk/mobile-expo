164a22d52fbb214262351e081e2d91cd8a1e8a71
peak <peakdrum@github.com>
2017-12-22 10:12:17 +0700

    upgraded redux persist

--- pages/register.js
+++ pages/register.js
@@ -1,53 +1,72 @@
 import React from 'react'
-import { bindActionCreators } from 'redux'
-import { reduxForm } from 'redux-form'
+import {bindActionCreators} from 'redux'
+import {reduxForm} from 'redux-form'
 import withRedux from "next-redux-wrapper"
 import store from '../lib/store'
 import loadFirebase from '../lib/database'
 import LoginForm from '../view/environment/AuthForm'
-import { signinWithFacebook, signinWithGoogle, signOut, registerWithEmail } from '../lib/handlers/authenticator'
-import { saveUser } from '../lib/actions/user'
-import { validateEmail, validatePassword, validatePasswordConfirmation } from '../lib/helpers/formvalidation'
+import {signinWithFacebook, signinWithGoogle, signOut, registerWithEmail} from '../lib/handlers/authenticator'
+import {saveUser} from '../lib/actions/user'
+import {validateEmail, validatePassword, validatePasswordConfirmation} from '../lib/helpers/formvalidation'
 
 class Login extends React.Component {
     async componentDidMount() {
         const auth = await loadFirebase('auth')
-        await auth.onAuthStateChanged( user => {user? this.props.saveUser(user): null}) 
+        await auth.onAuthStateChanged(user => {
+            user
+                ? this
+                    .props
+                    .saveUser(user)
+                : null
+        })
     }
 
     render() {
-        const { user, loginValues } = this.props
-        let validateEmailResult = validateEmail(loginValues ? loginValues.email : null)
-        let validatePasswordResult = validatePassword(loginValues ? loginValues.password : null)
-        let validatePasswordConfirmationResult = validatePasswordConfirmation(loginValues ? loginValues.password : null, loginValues? loginValues.passwordConfirm : null)
+        const {user, loginValues} = this.props
+        let validateEmailResult = validateEmail(loginValues
+            ? loginValues.email
+            : null)
+        let validatePasswordResult = validatePassword(loginValues
+            ? loginValues.password
+            : null)
+        let validatePasswordConfirmationResult = validatePasswordConfirmation(loginValues
+            ? loginValues.password
+            : null, loginValues
+            ? loginValues.passwordConfirm
+            : null)
         let statusArray = [validateEmailResult.status, validatePasswordResult.status, validatePasswordConfirmation.status]
-        let status = statusArray.indexOf('error') > 0 ? 'error' : statusArray.indexOf('warning') > 0 ? 'warning' : 'success'
-        return (
-            <LoginForm 
-                page="register" 
-                onClickFacebook={signinWithFacebook} 
-                onClickGoogle={signinWithGoogle}
-                signOut={signOut}
-                loggedIn={user.signedIn}
-				formValue={loginValues}
-                status={status}
-                helperText={[ ...validateEmailResult.errorText, ...validatePasswordResult.errorText, ...validatePasswordConfirmationResult.errorText]}
-                onSubmitEmail={registerWithEmail}
-            />
-        )
+        let status = statusArray.indexOf('error') > 0
+            ? 'error'
+            : statusArray.indexOf('warning') > 0
+                ? 'warning'
+                : 'success'
+        return (<LoginForm
+            page="register"
+            onClickFacebook={signinWithFacebook}
+            onClickGoogle={signinWithGoogle}
+            signOut={signOut}
+            loggedIn={user.signedIn}
+            formValue={loginValues}
+            status={status}
+            helperText={[
+            ...validateEmailResult.errorText,
+            ...validatePasswordResult.errorText,
+            ...validatePasswordConfirmationResult.errorText
+        ]}
+            onSubmitEmail={registerWithEmail}/>)
     }
 }
-Login = reduxForm({
-    form: 'login'
-})(Login)
+Login = reduxForm({form: 'login'})(Login)
 
 const mapStateToProps = state => ({
-    loginValues : state.form.login ? state.form.login.values : null,
+    loginValues: state.form.login
+        ? state.form.login.values
+        : null,
     user: state.user
 })
 
 const mapDispatchToProps = {
-	saveUser
+    saveUser
 }
 
-export default withRedux(()=>store,mapStateToProps, mapDispatchToProps)(Login)
+export default withRedux(() => store, mapStateToProps, mapDispatchToProps)(Login)
