3ed2a56267a8c73c725c2b05302f04dd1750a843
chonthicha <punthira.dits@gmail.com>
2018-01-26 15:06:42 +0700

    test image

--- view/environment/Product.js
+++ view/environment/Product.js
@@ -3,7 +3,7 @@ import Header from './Header'
 import ProductDisplay from '../ecosystems/ProductDisplay'
 
 export default props => <div>
-	<Head title={props.product.productName} image={props.product.productImages[0]} description={props.product.brandName} />
+	<Head title={props.product.productName} image="../../static/img/thumbnail.png" description={props.product.brandName} />
 	<Header/>
 	<ProductDisplay 
 		{...props}
