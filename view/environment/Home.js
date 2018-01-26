811b6cdbfcdf835fbadd78597db3e6dfc028b7bb
chonthicha <punthira.dits@gmail.com>
2018-01-26 21:41:40 +0700

    type video

--- view/environment/Home.js
+++ view/environment/Home.js
@@ -4,7 +4,7 @@ import CompanyExplained from '../ecosystems/CompanyExplained'
 
 export default ({changePage, user}) => 
 <div>
-  <Head title="Home" videoType="application/x-shockwave-flash" video="../../static/videos/video-1516960271.swf" description="แอพลิเคชั่นที่จะเปลี่ยนลูกค้าทุกคนของคุณเป็นตัวแทนจำหน่าย"/>
+  <Head title="Home" type="video" videoType="application/x-shockwave-flash" video="http://www.youtube.com/v/GFLGRidF04?version=3&autohide=1" description="แอพลิเคชั่นที่จะเปลี่ยนลูกค้าทุกคนของคุณเป็นตัวแทนจำหน่าย"/>
   <Header />
   <CompanyExplained/>
 </div>
