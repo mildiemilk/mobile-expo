b6c0ef1b9673609a8ac5b6fb4c9959f8fa390731
peak <peakdrum@github.com>
2018-01-30 14:53:48 +0700

    change so that hamburger work

--- view/ecosystems/HeaderMobile.js
+++ view/ecosystems/HeaderMobile.js
@@ -1,74 +1,51 @@
 import Router from 'next/router'
+import MediaQuery from 'react-responsive'
 import Sidebar from 'react-sidebar'
 import { Icon } from 'semantic-ui-react'
 import MenuButton from '../atoms/MenuButton'
 import Menu from '../atoms/Menu'
 import Item from '../atoms/Item'
 import menu from '../../static/json/menu.json'
-import BaseFlex from '../atoms/Flex'
-import styled from 'styled-components'
-import color from '../../static/json/color.json'
 
-const Flex = BaseFlex.extend`
-	position:absolute;
-	top:0;
-	z-index:100;
-	width: 100%;
-	background: ${color.primary1};
-	display:${props=>props.open? 'flex': 'none'};
-	transition: 0.3s;
-`
-
-const SmallScreenOnly = styled.div`
-	@media (min-width: 700px){
-		display: none;
-	}
-`
-
-const CloseButton = styled.span`
-	color: white;
-	font-size:1.2rem;
-`
 
 class HeaderMobile extends React.Component{
   constructor(props) {
     super(props);
 
     this.state = {
-      sidebarState: false
+      sidebarOpen: false
     }
   }
 
 
-	onSetSidebarOpen = state => {
-		this.setState({sidebarState: state});
+	onSetSidebarOpen = open => {
+		this.setState({sidebarOpen: open});
 	}
 
 	render(){
 		
 		return (
-			<SmallScreenOnly>
-				<div style={{fontSize:"3em", color:color.contrastHighlight, marginLeft:"10px", marginTop:"10px"}} onClick={()=>this.onSetSidebarOpen(true)}>
-					<i class="fas fa-bars"></i>
-				</div>
-				<Flex open={this.state.sidebarState}>
-				<CloseButton onClick={()=>this.onSetSidebarOpen(false)}>x</CloseButton>
-				{
-					menu.map( ({link, text}, key) => <Item 
-						key={key} 
-						onClick={()=>{
-							this.onSetSidebarOpen(false)
-							Router.push(link)}
-						}
-					>
-						{text}
-					</Item> 
-					)
-				}
-				</Flex>
-			</SmallScreenOnly>
+			<MediaQuery  maxDeviceWidth={700}>
+				<Sidebar sidebar={sidebarContent}
+					open={this.state.sidebarOpen}
+					onSetOpen={this.onSetSidebarOpen}
+				>
+
+				</Sidebar>
+				<MenuButton onClick={()=>this.onSetSidebarOpen(true)}><Icon name='content'/></MenuButton>
+			</MediaQuery>
 		)
 	}
 }
+
+const sidebarContent = 	
+<Menu height="100%">
+<img src="../../static/img/logo.png" width="50" height="50" />
+	<div>
+		{
+			menu.map( ({link, text}, key) => <Item key={key} onClick={()=>Router.push(link)}>{text}</Item> )
+		}
+	</div>
+</Menu>
 
 export default HeaderMobile
