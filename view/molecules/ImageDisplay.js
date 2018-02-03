e1c943e306719087e11115b4d0f5efd704ba28df
doschnn <detective-i@hotmail.com>
2018-02-03 13:09:55 +0700

    hover sub img completed

--- view/molecules/ImageDisplay.js
+++ view/molecules/ImageDisplay.js
@@ -1,7 +1,7 @@
 import Image from '../atoms/Image'
 import SquareWrapper from '../atoms/SquareWrapper'
 
-export default ({src, size, subImg}) =>
+export default ({src, size, subImg, onMouseOver, onMouseLeave}) =>
 <SquareWrapper maxSize={size}>
-    <Image src={src} margin="15px 0px" height="none" maxHeight="none" size={size || '400px'} subImg={subImg}/>
+    <Image src={src} onMouseOver={onMouseOver} onMouseLeave={onMouseLeave} margin="15px 0px" height="none" maxHeight="none" size={size || '400px'} subImg={subImg}/>
 </SquareWrapper>
--- view/organisms/DisplayImages.js
+++ view/organisms/DisplayImages.js
@@ -1,18 +1,39 @@
+import React from 'react'
 import Wrapper from '../atoms/ImageAtoms/ImageSectionWrapper'
 import ImageDisplay from '../molecules/ImageDisplay'
 import SubImageWrapper from '../atoms/ImageAtoms/SubImageWrapper'
 import DisplaySubImage from '../molecules/DisplaySubImage'
 import Flex from '../atoms/Flex'
 
-export default ({images = []}) => {
-    return (<Wrapper>
-        <ImageDisplay src={images[0]} />
-        <SubImageWrapper>
-        <Flex width="100%" justContent="center" direction="row" margin="25px 0px">
-            {images.slice(1).map(image => <DisplaySubImage src={image} size="100px" subimg />)}
-        </Flex>
-        </SubImageWrapper>
-    </Wrapper>)
+class DisplayImages extends React.Component {
+	constructor(props){
+		super(props)
+		this.state = {
+			imageIndex: 0
+		}
+    }
+
+    handleImageDisplay = (imageIndex) => this.setState({ imageIndex })
+
+    render() {
+        const { images = [] } = this.props
+        const { imageIndex } = this.state
+        return (<Wrapper>
+            <ImageDisplay src={images[imageIndex]} />
+            <SubImageWrapper>
+            <Flex width="100%" justContent="center" direction="row" margin="25px 0px">
+                {images.slice(1).map((image, index) => <DisplaySubImage 
+                    onMouseOver={() => this.handleImageDisplay(index+1)}
+                    onMouseLeave={() => this.handleImageDisplay(0)}
+                    src={image} 
+                    size="100px" 
+                    subimg />)}
+            </Flex>
+            </SubImageWrapper>
+        </Wrapper>)
+    }
 }
+
+export default DisplayImages
 
 
