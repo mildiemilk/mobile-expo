91066c2fec7b989b522a7b8137ec4e1b5b8b5dc0
Peak Mac <peakdrum@gmail.com>
2018-02-09 19:36:45 +0700

    make front page photo and also the cover photo

--- view/molecules/WebExplainCard.js
+++ view/molecules/WebExplainCard.js
@@ -6,13 +6,13 @@ import color from '../../static/json/color'
 import Button from '../atoms/Button'
 import Link from 'next/link'
 
-export default props => <Wrapper margin='20px 0' padding='0' minWidth='320px' maxWidth='700px'>
-	<div style={{height:'300px'}}>
-		<img />
+export default ({header,description,img}) => <Wrapper margin='20px 0' padding='0' minWidth='320px' maxWidth='700px'>
+	<div style={{height:'300px', display:'flex', alignItems:'flex-end'}}>
+		{img}
 	</div>
 	<WebExplainFooter>
-		<H3 color={color.lightPrimary}>{props.header}</H3>
-		<p style={{color:color.lightPrimary}}>{props.description}</p>
+		<H3 color={color.lightPrimary}>{header}</H3>
+		<p style={{color:color.lightPrimary}}>{description}</p>
 		<Link href='register'>
 			<Button>สมัครเลย</Button>
 		</Link>
