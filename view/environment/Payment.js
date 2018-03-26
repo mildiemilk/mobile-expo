b9199cea1117c43a176e0b2e11b8013457b1a112
doschnn <suchanun.intanon@gmail.com>
2018-03-26 13:49:50 +0700

    fixed conditions tabs

--- view/environment/Payment.js
+++ view/environment/Payment.js
@@ -13,17 +13,19 @@ export const Tabs = props =>  {
 		{
 			buttonLabel:'โอนผ่านธนาคาร',
 			component:<BankAccountsForTransfer {...props}/>
-		},
-		{
-			buttonLabel: 'จ่ายผ่านบัตรเครดิต/เดบิต',
-			component: <CreditCard {...props}/>
 		}
 	]
-	if(props.transaction.price >= 2000) 
-		tabs.push({
-			buttonLabel: 'Internet Banking',
-			component: <InternetBanking {...props}/>
-		})
+	if(props.transaction.price >= 20)
+		tabs.push(
+			{
+				buttonLabel: 'จ่ายผ่านบัตรเครดิต/เดบิต',
+				component: <CreditCard {...props}/>
+			},
+			{
+				buttonLabel: 'Internet Banking',
+				component: <InternetBanking {...props}/>
+			}
+		)
 	return tabs
 }
 
